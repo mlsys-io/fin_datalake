46e27824c107b04213f9d3705c23966e53bfdad9
Zhengyuan Su <su.zhengyuan@u.nus.edu>
2025-08-28 10:14:12 +0800

    Add restart command for ollama

--- bash-scripts/ollama.sh
+++ bash-scripts/ollama.sh
@@ -3,3 +3,6 @@ helm repo update
 helm install ollama ollama-helm/ollama \
   --namespace ollama \
   --create-namespace -f helm-config/ollama.yaml
+
+# To restart after ollama shuts down
+kubectl rollout restart deployment/ollama --namespace ollama
