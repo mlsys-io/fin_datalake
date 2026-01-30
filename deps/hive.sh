b6e88fa489d8b2dcae54c8c58400b0e1ddff03e9
Garret <lzx122603@gmail.com>
2026-01-30 13:42:20 +0800

    Hive deployment script

--- deps/hive.sh
+++ deps/hive.sh
@@ -6,19 +6,19 @@ MINIO_CERT=${4-$HOME/.mc/certs/CAs/public.crt}
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-kubectl get namespace | grep -q "^hive " || kubectl create namespace hive # Create namespace if not exists
+kubectl get namespace | grep -q "^etl-storage " || kubectl create namespace etl-storage # Create namespace if not exists
 
-helm install hms-db bitnami/postgresql -n hive -f $SCRIPT_DIR/hive/hms-db.yaml
+helm install hms-db bitnami/postgresql -n etl-storage -f $SCRIPT_DIR/hive/hms-db.yaml
 
 echo "Deployed PostgreSQL for Hive Metastore"
 
-kubectl delete configmap minio-ca -n hive 2>/dev/null
-kubectl create configmap minio-ca -n hive --from-file=public.crt=$MINIO_CERT
+kubectl delete configmap minio-ca -n etl-storage 2>/dev/null
+kubectl create configmap minio-ca -n etl-storage --from-file=public.crt=$MINIO_CERT
 echo "Created ConfigMap for MinIO CA certificate"
 
-kubectl delete secret minio-creds -n hive 2>/dev/null
-kubectl create secret generic minio-creds -n hive \
+kubectl delete secret minio-creds -n etl-storage 2>/dev/null
+kubectl create secret generic minio-creds -n etl-storage \
   --from-literal=username=$MINIO_USERNAME --from-literal=password=$MINIO_PASSWORD
 echo "Created Secret for MinIO credentials"
 
-helm install hms $SCRIPT_DIR/hive/hms-chart -n hive --set s3.endpoint=$MINIO_ENDPOINT
+helm install hms $SCRIPT_DIR/hive/hms-chart -n etl-storage --set s3.endpoint=$MINIO_ENDPOINT
