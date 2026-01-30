de88a2f12079f516554e8716ba6165ebc1e0a0a1
Garret <lzx122603@gmail.com>
2026-01-30 13:59:57 +0800

    Change kuberay namespace

--- deps/kuberay.sh
+++ deps/kuberay.sh
@@ -1,3 +1,3 @@
 helm repo add kuberay https://ray-project.github.io/kuberay-helm
 helm repo update
-helm install kuberay-operator kuberay/kuberay-operator
+helm install kuberay-operator kuberay/kuberay-operator -n etl-compute
