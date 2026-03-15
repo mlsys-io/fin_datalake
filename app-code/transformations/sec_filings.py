a81a634fce096df70fa5bfdbe86e495fb0a80d6f
Garret <lzx122603@gmail.com>
2026-03-15 13:26:25 +0800

    Remove legacy code

--- app-code/transformations/sec_filings.py
+++
@@ -1,9 +0,0 @@
-import ray.data as rd
-from .helper import process_sec_file, regex_extractor
-from ioutils import MAX_CONCURRENCY
-
-def transform_sec(ds: rd.Dataset) -> rd.Dataset:
-    return ds.rename_columns({"path": "_source_path"})\
-        .add_column("symbol", regex_extractor(r"/SEC-Filings/([^/]+)/"), concurrency=MAX_CONCURRENCY)\
-        .add_column("content", lambda df: df["text"].apply(process_sec_file), concurrency=MAX_CONCURRENCY)\
-        .drop_columns(["text"])
